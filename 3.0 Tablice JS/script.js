dfa697c7eb35b8e188576dcda8cb76f74351e80d
skaznik <skaznik@wszib.edu.pl>
2019-12-01 10:38:02 +0100

    _cd

--- 3.0 Tablice JS/script.js
+++ 3.0 Tablice JS/script.js
@@ -6,3 +6,14 @@ console.log(numberArray);
 console.log(stringArray);
 console.log(booleanArray);
 
+console.log(numberArray[1]);
+
+stringArray[2]= 'cztery';
+console.log(stringArray);
+
+booleanArray.push(false);
+console.log(booleanArray);
+
+console.log(stringArray.includes('pięć'));
+
+console.log(booleanArray.length);
