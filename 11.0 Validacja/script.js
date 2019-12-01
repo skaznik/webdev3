f7862e8c28e4f23267128cba6c3258e9e8609efc
skaznik <skaznik@wszib.edu.pl>
2019-12-01 15:35:19 +0100

    ActionListener

---
+++ 11.0 Validacja/script.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  let form = document.getElementById('formularz');
+  form.addEventListener('submit', function (e) {
+     e.preventDefault();
+
+     let wartosc = document.getElementById('name').value;
+     //console.log(wartosc);
+     if (wartosc.length >= 3) {
+         console.log('Poprawna wartość');
+         document.getElementById('error').setAttribute('hidden', '');
+         e.currentTarget.submit();
+     } else {
+         document.getElementById('error').removeAttribute('hidden');
+     }
+
+  });
+
+
+};
