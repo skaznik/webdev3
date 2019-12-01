a4353341c5e87b80a1f177be56f77c4f40770f76
skaznik <skaznik@wszib.edu.pl>
2019-12-01 16:08:22 +0100

    JQuery

---
+++ 12.0 JQuery/12.1 Model DOM/script.js
@@ -0,0 +1,27 @@
+/* sprawdzamy, czy model DOM jest gotowy - BARDZO WAŻNE!!!! */
+window.onload = function () {
+    /* wyszukiwanie po id */
+   // document.getElementById('container').style.border = '5px solid red';
+    /* wyszukiwanie po klasie CSS */
+   // document.querySelector('.box').style.border = '3px solid red';
+
+    //let array = document.getElementsByClassName('box');
+    //for (let i = 0; i < array.length; i++) {
+    //array[i].style.border = '2px solid blue';
+    //}
+    /* szybsza metoda */
+    //let array = document.getElementsByClassName('box');
+    //for (element of array) {
+      //  element.style.border = '5px solid green'
+   // }
+    /* wyszukiwanie po tagach */
+    //let array = document.getElementsByTagName('div');
+    //for (element of array) {
+    //    element.style.border = '5px solid green'
+    //}
+    //document.getElementById('special').parentNode.style.border
+   // document.getElementById('special').previousElementSibling.style.border = '5px solid red';
+   // document.getElementById('special').nextElementSibling.style.border = '5px solid red';
+    //document.getElementById('special').previousElementSibling.style.border = '5px solid red';
+    document.getElementById('special').lastElementChild.style.border = '5px solid red';
+};
